81e9f3a4173a450451337a11d4c86e864fbf3be9
DoctorLai <dr.zhihua.lai@gmail.com>
2026-02-04 15:31:57 +0000

    Update

--- src/levels/index.ts
+++ src/levels/index.ts
@@ -3,11 +3,7 @@ import level1 from "./world1-001.json";
 import level2 from "./world1-002.json";
 import level3 from "./world1-003.json";
 import level4 from "./world1-004.json";
-import level5 from "./world1-005.json";
 import level6 from "./world1-006.json";
-import level7 from "./world1-007.json";
-import level8 from "./world1-008.json";
 import level9 from "./world1-009.json";
-import level10 from "./world1-010.json";
 
-export const LEVELS: LevelJson[] = [level1, level2, level3, level4, level5, level6, level7, level8, level9, level10];
+export const LEVELS: LevelJson[] = [level1, level2,level3, level4, level6, level9];
